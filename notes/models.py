e54eea774bb9df4c66cb0d96545ba6764aec31f2
ziyad <ziyad.alnawfal1@gmail.com>
2024-06-03 11:46:33 +0100

    set up DB

--- notes/models.py
+++ notes/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Notes(models.Model):
+    text = models.TextField()
+    title = models.CharField(max_length=125)
+    date_created = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
